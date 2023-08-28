ca4921bef7e24c47a99367c0ad02c2961870c3cc
LemoIngabire <leonlemoningabire@gmail.com>
2023-08-28 04:13:07 +0200

    updates

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,98 +1,101 @@
-
 import 'package:flutter/material.dart';
 import 'package:skill_link/widgets/groupchat_widget.dart';
 import 'package:skill_link/widgets/personalchat_widget.dart';
 
-
-class ChatPage extends StatefulWidget{
+class ChatPage extends StatefulWidget {
   @override
-  State<ChatPage> createState()=>_ChatPage();
+  State<ChatPage> createState() => _ChatPage();
 }
 
-class _ChatPage extends State<ChatPage>{
-  int _buttonIndex =0;
-  final _scheduleWidgets =[
+class _ChatPage extends State<ChatPage> {
+  int _buttonIndex = 0;
+  final _chatWidgets = [
     PersonalChat(),
     GroupChat(),
-
   ];
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child :SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.only(top: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text('Chats',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500
-                ),),),
-            SizedBox(height: 5,),
-            Container(
-              padding: EdgeInsets.all(5),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: (){
-                      setState(() {
-                        _buttonIndex =0;
-                      });
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical:12 ,horizontal: 25),
-                      decoration: BoxDecoration(
-                          color: _buttonIndex ==0 ? Colors.deepOrange : Colors.transparent,
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Text('Personal',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                            color: _buttonIndex == 0? Colors.white:Colors.black38
-                        ),),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: (){
-                      setState(() {
-                        _buttonIndex =1;
-                      });
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical:12 ,horizontal: 25),
-                      decoration: BoxDecoration(
-                          color: _buttonIndex ==1 ? Colors.deepOrange : Colors.transparent,
-                          borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Text('Groups',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 10,
-                            color: _buttonIndex == 1? Colors.white:Colors.black38
-                        ),),
-                    ),
-                  ),
-
-                ],
-                // ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 15),
+            child: Text(
+              'Chats',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(height: 10,),
-            _scheduleWidgets[_buttonIndex],
-          ],
-        ),
+          ),
+          SizedBox(height: 5),
+          Container(
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _buttonIndex = 0;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: _buttonIndex == 0 ? Colors.deepOrange : Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      'Personal',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10,
+                        color: _buttonIndex == 0 ? Colors.white : Colors.black38,
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _buttonIndex = 1;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: _buttonIndex == 1 ? Colors.deepOrange : Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      'Groups',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10,
+                        color: _buttonIndex == 1 ? Colors.white : Colors.black38,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: SingleChildScrollView(
+              child: _chatWidgets[_buttonIndex],
+            ),
+          ),
+        ],
       ),
-      )
     );
   }
 }
